6c4ec21ee2fb10fad5bb81ea46bbb4d128f3ff9e
Sarah <98801126+SarahARauf@users.noreply.github.com>
2024-11-08 17:12:55 +0800

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -10,16 +10,16 @@ public:
         vector<int> ansVector;
 
 
-        for (int i=0; i < nums.size(); i++)
+        for (int i=0; i < nums.size(); i++) //Using a loop to put all the elements in nums into numsHash, where the element is the key and the index is the value
         {
             numsHash[nums[i]] = i;
         }
 
-        for (int i=0; i < nums.size(); i++)
-        {
+        for (int i=0; i < nums.size(); i++) 
+        { 
 
-            int ans = target-nums[i];
-            if (numsHash.find(ans) != numsHash.end() && i != numsHash[ans])
+            int ans = target-nums[i]; //mminus target from current number to get the answer that we are attempting to look for
+            if (numsHash.find(ans) != numsHash.end() && i != numsHash[ans]) //try to find the answer in the hashmap, making sure that the indexes are not the same (so as to not add the same element into the answer vector)
             {
                 ansVector.push_back(i);
                 ansVector.push_back(numsHash[ans]);
